c06c01b87be1ac31523bb7e6fc9229e6171eda0a
hamilkarr <12uclock@gmail.com>
2023-05-30 01:28:16 +0900

    함수 오버로드 완료

--- dist/app.js
+++ dist/app.js
@@ -10,54 +10,6 @@ function add(a, b) {
     }
     return a + b;
 }
-function printEmployeeInformation(emp) {
-    console.log('Name: ' + emp.name);
-    if ('privileges' in emp) {
-        console.log('Privileges: ' + emp.privileges);
-    }
-    if ('startDate' in emp) {
-        console.log('Start Date: ' + emp.startDate);
-    }
-}
-printEmployeeInformation(e1);
-class Car {
-    drive() {
-        console.log('Driving...');
-    }
-}
-class Truck {
-    drive() {
-        console.log('Driving a truck...');
-    }
-    loadCargo(amount) {
-        console.log('Loading cargo...' + amount);
-    }
-}
-const v1 = new Car();
-const v2 = new Truck();
-function useVehicle(vehicle) {
-    vehicle.drive();
-    if (vehicle instanceof Truck) {
-        vehicle.loadCargo(1000);
-    }
-}
-useVehicle(v1);
-useVehicle(v2);
-function moveAnimal(animal) {
-    let speed;
-    switch (animal.type) {
-        case 'bird':
-            speed = animal.flyingSpeed;
-            break;
-        case 'horse':
-            speed = animal.runningSpeed;
-            break;
-    }
-    console.log('Moving at speed: ' + speed);
-}
-moveAnimal({ type: 'bird', flyingSpeed: 10 });
-const userInputElement = document.getElementById('user-input');
-if (userInputElement) {
-    userInputElement.value = 'Hi there!';
-}
+const result = add('Max', ' Schwarz');
+result.split(' ');
 //# sourceMappingURL=app.js.map
