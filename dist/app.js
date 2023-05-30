a0e67e7227d6542a74d38519dc3d13db411c7884
hamilkarr <12uclock@gmail.com>
2023-05-30 23:51:14 +0900

    제네릭 시작

--- dist/app.js
+++ dist/app.js
@@ -1,25 +1,3 @@
 "use strict";
-var _a;
-const e1 = {
-    name: 'Max',
-    privileges: ['create-server'],
-    startDate: new Date(),
-};
-function add(a, b) {
-    if (typeof a === 'string' || typeof b === 'string') {
-        return a.toString() + b.toString();
-    }
-    return a + b;
-}
-const result = add('Max', ' Schwarz');
-result.split(' ');
-const fetchUserData = {
-    id: 'u1',
-    name: 'Max',
-    job: { title: 'CEO', description: 'My own company' },
-};
-console.log((_a = fetchUserData === null || fetchUserData === void 0 ? void 0 : fetchUserData.job) === null || _a === void 0 ? void 0 : _a.title);
-const userInput = '';
-const storedData = userInput !== null && userInput !== void 0 ? userInput : 'DEFAULT';
-console.log(storedData);
+const names = ['Max', 'Manuel'];
 //# sourceMappingURL=app.js.map
