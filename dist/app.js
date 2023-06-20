f652106290fc27fee21372a34620a3e28a3cb9c3
hamilkarr <12uclock@gmail.com>
2023-06-21 08:32:10 +0900

    복습중

--- dist/app.js
+++ dist/app.js
@@ -5,9 +5,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log('Logging...');
-    console.log(constructor);
+function Logger(logString) {
+    console.log('LOGGER FACTORY');
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+function WidthTemplate(template, hookId) {
+    console.log('TEMPLATE FACTORY');
+    return function (_) {
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+            hookEl.innerHTML = template;
+        }
+    };
 }
 let Person = class Person {
     constructor() {
@@ -16,8 +29,9 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    Logger('LOGGING'),
+    WidthTemplate('<h1>My Person Object</h1>', 'app')
 ], Person);
-const pers = new Person();
-console.log(pers);
+const person = new Person();
+console.log(person);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
-function Logger(constructor: Function) {
-    console.log('Logging...');
-    console.log(constructor);
+function Logger(logString: string) {
+    console.log('LOGGER FACTORY');
+    return function (constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    };
 }
 
-@Logger
+function WidthTemplate(template: string, hookId: string) {
+    console.log('TEMPLATE FACTORY');
+    return function (_: Function) {
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        if (hookEl) {
+            hookEl.innerHTML = template;
+        }
+    };
+}
+
+// @Logger('LOGGING - PERSON')
+@Logger('LOGGING')
+@WidthTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = 'Max';
     constructor() {
@@ -11,5 +27,5 @@ class Person {
     }
 }
 
-const pers = new Person();
-console.log(pers);
+const person = new Person();
+console.log(person);
